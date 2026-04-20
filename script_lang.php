12286155498b15120b2f6b65fc59f9b25ee7bd45
Fana2003 <168146307+Fana2003@users.noreply.github.com>
2026-04-20 13:48:49 +0300

    Fetch and display content from a remote URL

--- script_lang.php
+++ script_lang.php
@@ -1,5 +1,18 @@
 <?php
-echo "<pre>";
-system('cat index.php');
-echo "</pre>";
+
+$url = "http://challenge01.root-me.org/web-serveur/ch13/?lang/index.php";
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$code = curl_exec($ch);
+curl_close($ch);
+
+if ($code === false) {
+    echo "Erreur de récupération.";
+} else {
+    echo $code;
+}
+
 ?>
