5aaeb9cf87179a4fa95de4d4da9ac338b1e5ebe0
Fana2003 <168146307+Fana2003@users.noreply.github.com>
2026-04-20 13:32:01 +0300

    Change method to display index.php content
    Replace file_get_contents with system call to display index.php content.

--- script_lang.php
+++ script_lang.php
@@ -1,8 +1,5 @@
 <?php
-$fichier = 'index.php';
-// Lit le contenu du fichier dans une chaîne
-$code = file_get_contents($fichier); 
-
-// Affiche le code en convertissant les balises PHP en texte visible
-echo "<pre>" . htmlspecialchars($code) . "</pre>";
+echo "<pre>";
+system('cat index.php');
+echo "</pre>";
 ?>
