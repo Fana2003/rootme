65386bed1221c3d70285d53bed087be9bd023193
Fana2003 <168146307+Fana2003@users.noreply.github.com>
2026-04-20 13:53:45 +0300

    Update script_lang.php

--- script_lang.php
+++ script_lang.php
@@ -1,18 +1,19 @@
 <?php
 
-$url = "http://challenge01.root-me.org/web-serveur/ch13/?lang/index.php";
+$url = "http://eric.org/web-serveur/ch13/index.php";
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
 $code = curl_exec($ch);
-curl_close($ch);
 
 if ($code === false) {
-    echo "Erreur de récupération.";
+    echo "Erreur de récupération : " . curl_error($ch);
 } else {
     echo $code;
 }
+
+curl_close($ch);
 
 ?>
